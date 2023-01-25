c00fad5c02773e9e5b4b970d129cd6acbfe05561
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-01-25 01:41:29 -0600

    update: miscellaneous css

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('pre code').forEach((el) => {
     hljs.highlightElement(el);
   });
+
+
+  $(document).on('click', function(e){
+    $("span#dark-mode").click(function(){
+             var element = document.body;
+                element.classList.toggle("dark-mode");
+  });
+
+
 });
+
+  });
